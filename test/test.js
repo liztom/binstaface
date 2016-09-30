b51ab548ae03a9665c39bec72836447c302eda78
Liz Tom <liz@bitovi.com>
2016-09-30 09:36:41 -0700

    simple routes

--- test/test.js
+++ test/test.js
@@ -1,9 +1,11 @@
 /* global it describe */
 
 const assert = require('assert');
+const request = require('request');
 
 describe('my app test', function myAppTest() {
-  it('does something', function doesSomethingTest() {
-    assert.ok(true, 'Did something');
+  it('gets all the messages', function doesSomethingTest() {
+    const messages = { 0: 'hey', 1: 'my message', 2: 'message 3' };
+    assert.equal(response.length, Object.keys(messages).length);
   });
 });
